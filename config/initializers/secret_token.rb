4e0275aad28201c8172f7f102c3596819930b093
Jmp0Bit <jmp0bit@gmail.com>
2011-11-22 00:23:24 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '80c3c137d6949c174db6164f7d5279c5256a5be1bbd2a63e8e7c764122ab219c021707d8d0fa8eb83d308a864841d05bb3d48a819f859a04bf2852d333e94015'
